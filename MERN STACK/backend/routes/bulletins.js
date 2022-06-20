13d3e91d39f91f6175505059d67b0698bd4607d7
SteeshMcK <stesha.norstrom@pcc.edu>
2022-06-20 13:51:31 -0700

    create routes folder and file

---
+++ MERN STACK/backend/routes/bulletins.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+// invoke express router and assign to variable
+const router = express.Router()
+
+router.get = ('/', () => {})
+
+// export router
+module.exports = router
+
